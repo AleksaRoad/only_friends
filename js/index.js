d05b5fa76ea0fc94f3099cf6da19df45d18fefdd
AleksaRoad <aleksaroad@gmail.com>
2023-03-13 21:49:45 +0300

    first commit

---
+++ js/index.js
@@ -0,0 +1,13 @@
+// import './sass';
+
+// function component(text) {
+//   const element = document.createElement('h1');
+//   element.textContent = text;
+//   return element;
+// }
+
+// document.body.prepend(component('Проект собран на Webpack'));
+
+const menu_item= document.querySelectorAll(".menu__list-item");
+
+menu_item[0].classList.add('active')
