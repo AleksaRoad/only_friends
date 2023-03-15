fa3ca0c9e49a21b7bddd9f99a63f37dc7969e9c8
AleksaRoad <aleksaroad@gmail.com>
2023-03-16 01:29:15 +0300

    feat: section about buttons section our-friends 320px

--- js/index.js
+++ js/index.js
@@ -8,6 +8,12 @@
 
 // document.body.prepend(component('Проект собран на Webpack'));
 
-const menu_item= document.querySelectorAll(".menu__list-item");
+const menu_item = document.querySelectorAll(".menu__list-item");
+const burger_btn = document.querySelector('.burger__btn');
 
 menu_item[0].classList.add('active')
+
+burger_btn.addEventListener('click', ()=>{
+  burger_btn.classList.toggle('active');
+}
+)
