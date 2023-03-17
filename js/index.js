67170a7c9d7ede07712d05455bf77a8e3a6b8a2f
AleksaRoad <aleksaroad@gmail.com>
2023-03-17 23:25:05 +0300

    feat: section in-addition and footer, fix bg header and responsive

--- js/index.js
+++ js/index.js
@@ -1,12 +1,3 @@
-// import './sass';
-
-// function component(text) {
-//   const element = document.createElement('h1');
-//   element.textContent = text;
-//   return element;
-// }
-
-// document.body.prepend(component('Проект собран на Webpack'));
 
 const menu_item = document.querySelectorAll(".menu__list-item");
 const burger_btn = document.querySelector('.burger__btn');
